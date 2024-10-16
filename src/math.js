5dcad54c29cd7dee31fb5beee09897d31bb2ba4d
unknown <22214290007@ACADEMICO.IESB.BR>
2024-10-16 20:09:54 -0300

    Desenvolvendo funções conforme os testes

--- src/math.js
+++ src/math.js
@@ -1,11 +1,31 @@
-function fatorial() {
+function fatorial(n) {
+  if (typeof n !== "number" || !Number.isInteger(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n < 0) return null;
+  if (n === 0 || n === 1) return 1;
+  return n * fatorial(n - 1);
 }
-function fibonnacci() {
+
+
+function fibonacci(n) {
+  if (typeof n !== "number" || !Number.isInteger(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
-function ehPrimo() {
+
+
+function ehPrimo(n) {
+  if (typeof n !== "number" || !Number.isInteger(n)) {
+    throw new TypeError("O argumento deve ser um número válido");
+  }
+  if (n <= 1) return false;
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
-module.exports = {
-  fatorial,
-  fibonnacci,
-  ehPrimo
-}
+
+module.exports = { fatorial, fibonacci, ehPrimo };
